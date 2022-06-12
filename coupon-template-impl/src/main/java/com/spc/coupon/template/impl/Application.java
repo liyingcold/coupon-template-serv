e6b11023541ba9472eb744b21187accfd06ca7a0
liyingcold <2608953051@qq.com>
2022-06-12 22:50:00 +0800

    配置nacos

--- coupon-template-impl/src/main/java/com/spc/coupon/template/impl/Application.java
+++ coupon-template-impl/src/main/java/com/spc/coupon/template/impl/Application.java
@@ -2,12 +2,14 @@ package com.spc.coupon.template.impl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@ComponentScan(basePackages = {"com.spc"})
+@ComponentScan(basePackages = {"com.spc.coupon.template"})
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
